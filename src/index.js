d66a3293bc2e1c340f3f4878b50fb1406ada166b
kent10ou <kent10ou@gmail.com>
2017-10-05 12:45:20 -0700

    updating react-router-dom pkg, adding BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+  render() { <div>Hello</div>}
+}
+
+class Goodbye extends React.Component {
+  render() { <div>Goodbye</div>}
+}
+
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <Route path="/hello" component={Hello} />
+      <Route path="/goodbye" component={Goodbye} />
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
